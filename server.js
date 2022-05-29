7e7dba02ce109bb4f2cb903c3aa04709013f4fa5
vinodk79 <106461190+vinodk79@users.noreply.github.com>
2022-05-29 01:24:27 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http')
+
+var port = 4001
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.end('Hi I am farhan ali\n')
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
